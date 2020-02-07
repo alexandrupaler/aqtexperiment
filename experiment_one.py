52b89e34a4a7ad9df04520148ea7028222a8ee4d
alexandrupaler <alexandrupaler@gmail.com>
2020-02-06 22:44:23 -0600

    working with aqt cloud

--- experiment_one.py
+++ experiment_one.py
@@ -1,7 +1,11 @@
+from os import getenv
+
 import cirq
 
-from cirq.aqt import aqt_device
-from cirq.aqt import AQTSimulator
+import numpy as np
+
+from cirq.aqt import AQTSampler, AQTSimulator
+
 
 import optimizers.measurements_last as ml
 import optimizers.replace_opposite as ro
@@ -49,7 +53,7 @@ def gen_s_dist_circuit(cirq_circuit, qubits):
     # round of X measurements on the qubits numbered 1...7
     for i in range(1, 8):
         cirq_circuit.append(cirq.ops.H(qubits[i]))
-        cirq_circuit.append(cirq.ops.MeasurementGate(1, key=str(i)).on(qubits[i]))
+        cirq_circuit.append(cirq.measure(qubits[i]))
 
     return
 
@@ -72,7 +76,15 @@ def compute_parity(config, results) -> []:
 
         # which order are the results returned?
         for index in config:
-            parity += int(results.measurements[str(index)][repetition])
+            parb = 0
+            # if is_cloud_sim:
+            #     parb += int(
+            #         results.measurements[measurement_key][repetition][index])
+            # else:
+            parb += int(
+                results.measurements[str(index)][repetition])
+
+            parity += parb
 
         # 0 -> 1 (even) and 1 -> -1 (odd)
         par.append(1 - 2 * (parity % 2))
@@ -94,42 +106,80 @@ def generate_stats(results, parities):
     # for each result returned from the simulation
     for repetition in range(results.repetitions):
 
-        totalpar = 1
-        str_par = ""
-        for parity in parities.keys():
-            if str(parity)[0] != "_":
+        total_bit_all_plaques = 1
+        str_plaque_par = ""
+        for plaq_parity in parities.keys():
+            if str(plaq_parity)[0] != "_":
                 # If this is not a hidden key, such as _byprod
-                par_bit = parities[parity][repetition]
-                totalpar *= par_bit
-                str_par += str(par_bit)
+                par_bit = parities[plaq_parity][repetition]
+                total_bit_all_plaques *= par_bit
+                str_plaque_par += str(par_bit)
 
-        str_meas = ""
+        qubits_meas_res = ""
         for k in results.measurements.keys():
-            str_meas += str(results.measurements[k][repetition][0])
+            # I am not sure why the result is store in a 1D array
+            qubits_meas_res += str(results.measurements[k][repetition][0])
 
-        print(str(repetition) + ". " + str_meas + " p:" + str_par + " total:" + str(totalpar))
+        print(str(repetition) + ". " + qubits_meas_res
+              + " plaq_par:" + str_plaque_par
+              + " total:" + str(total_bit_all_plaques))
 
         # these are the error-corrected states
         # store them in a map
-        if totalpar == 1:
+        if total_bit_all_plaques == 1:
             # does the state require a pauli correction?
+            # create an entry
             entry = [0, parities["_byprod"][repetition]]
-            if str_meas in stats.keys():
-                entry = stats[str_meas]
+            # if the entry exists retrieve it
+            if qubits_meas_res in stats.keys():
+                entry = stats[qubits_meas_res]
+            # update the count
             entry[0] += 1
 
-            stats[str_meas] = entry
+            # this measurement pattern was observed according
+            # to the data stored in the entry
+            stats[qubits_meas_res] = entry
         else:
+            # this did not go well
             print("not even parity")
 
+    """
+    What is the total number of times the even total parity was observed?
+    """
     total = 0
     for result in stats:
         total += stats[result][0]
 
-    print("Total states where parity of parities is even " + str(total))
+    print("Total states where parity of plaquetes is even " + str(total))
 
     return stats
 
+
+def reformat_results(results):
+    """
+        AQTSampler stores repetition-qubit instead of qubit-repetition
+        This method inverts rows with columns to be compatible with Cirq
+    :param results:
+    :return: inverted dictionary of measurement results
+    """
+    # The key by which the results are stored
+    measurements = results.measurements["m"]
+
+    # The measurement keys will be the index of the qubits
+    nr_qubits = len(measurements[0])
+
+    new_dict = {}
+    for i in range(nr_qubits):
+        entry = np.ndarray(shape=(results.repetitions, 1), dtype=np.int)
+        for j in range(results.repetitions):
+            entry[j] = int(measurements[j][i])
+
+        new_dict[str(i)] = entry
+
+    # Access private/hidden, because measurements is property
+    results._measurements = new_dict
+
+    return results
 
 def main():
     print("Hello World!")
@@ -138,37 +188,46 @@ def main():
     num_qubits = 8
 
     # Initialise the device and the qubits
-    aqt_comp, aqt_qubits = aqt_device.get_aqt_device(num_qubits)
+    aqt_comp, aqt_qubits = cirq.aqt.aqt_device.get_aqt_device(num_qubits)
 
     # Create the circuit
-    cirq_circuit = cirq.Circuit(device = aqt_comp)
+    circuit = cirq.Circuit(device = aqt_comp)
 
     # Should send a parameter/dict for the gate that I wish to transversally apply on the nodes
-    gen_s_dist_circuit(cirq_circuit, qubits=aqt_qubits)
+    gen_s_dist_circuit(circuit, qubits=aqt_qubits)
 
     # Et voila! after the circuit has been automatically decomposed
     # because the IonDevice (and devices in general) call decompose_operation and decompose_circuit
-    print(cirq_circuit)
+    print(circuit)
 
     # is there an optimiser that cancels neighbouring gates of opposite angles?
     pointoptimizer = ro.ReplaceOppositeRotations()
-    pointoptimizer.optimize_circuit(cirq_circuit)
+    pointoptimizer.optimize_circuit(circuit)
     # print(cirq_circuit)
 
     # move measurements to the last moment
     measurementsopt = ml.MoveMeasurementsLastPass()
-    measurementsopt.optimize_circuit(cirq_circuit)
+    measurementsopt.optimize_circuit(circuit)
     # print(cirq_circuit)
 
 
-    # Instantiate a simulator
-    # no_noise=True forces the underlying simulator to be a DensityMatrix without noise
-    # Useful for checking correctness of circuit
-    no_noise = True
-    # Take the AQTSimulator
-    aqt_sim = AQTSimulator(num_qubits=num_qubits, circuit = cirq_circuit, simulate_ideal=no_noise)
-    # internally this runs a cirq.study
-    results = aqt_sim.simulate_samples(repetitions=100)
+    # # Instantiate a simulator
+    # # no_noise=True forces the underlying simulator to be a DensityMatrix without noise
+    # # Useful for checking correctness of circuit
+    # no_noise = True
+    # # Take the AQTSimulator
+    # aqt_sim = cirq.aqt.AQTSimulator(num_qubits=num_qubits, circuit = circuit, simulate_ideal=no_noise)
+    # # internally this runs a cirq.study
+    # results = aqt_sim.simulate_samples(repetitions=100)
+    # print(results)
+    #
+    aqt_sampler = cirq.aqt.AQTSampler(
+        remote_host = "https://gateway.aqt.eu/marmot/sim/noise-model-1",
+        access_token = str(getenv("AQT_TOKEN"))
+    )
+
+    results = aqt_sampler.run(circuit, repetitions=100)
+    results = reformat_results(results)
     print(results)
 
     # perform analysis of measurement results
@@ -180,17 +239,17 @@ def main():
     multi_body_meas["plaq3"] = [1, 4, 6, 7]
     # this indicates if the code word has a flipped sign
     multi_body_meas["_byprod"] = list(range(1, 8))
-    print("Multi bodty measurements are")
+    print("Multi body measurements are")
     print(multi_body_meas)
 
     plaq_parity = {}
     for plaq in multi_body_meas.keys():
-        plaq_parity[plaq] = compute_parity(multi_body_meas[plaq], results)
+        plaq_parity[plaq] = compute_parity(multi_body_meas[plaq],
+                                           results)
 
     stats = generate_stats(results, plaq_parity)
     print("Statistics")
     print(stats)
-
 
 
 if __name__ == "__main__":
